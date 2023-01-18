4460b4d6865ce6f0dfabbf4519a1fb93f3f6a3f5
Tahir Dibirov II <tahirdibirovresearch@gmail.com>
2023-01-18 18:34:54 +0400

    INIT

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { FC, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <div className="shortener">
+      <h2>Get Your URL Shortened</h2>
+        <div className="controller">
+        <input type="text" name="" id="" placeholder='URL' />
+        <button>Get Link</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Services/URLService.ts
@@ -0,0 +1,15 @@
+import IURL from "./URLModel"
+
+export class getShorten{
+
+    constructor(URL){
+        this.URL = URL
+    }
+
+
+    async getURL: Promise<IURL> ()=>{
+
+    }
+
+
+}
